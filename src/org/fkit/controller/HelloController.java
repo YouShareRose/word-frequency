1acfd3b129609eb478cc2b32f342718e9129576d
tqw <941375265@qq.com>
2019-02-21 22:16:13 +0800

    基于注解的控制器

--- src/org/fkit/controller/HelloController.java
+++ src/org/fkit/controller/HelloController.java
@@ -1,13 +1,16 @@
 package org.fkit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloController implements Controller {
-    @Override
+@Controller
+public class HelloController  {
+
+    @RequestMapping(value = "/hello")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("handleRequest 被调用");
         ModelAndView mv = new ModelAndView();
