57478e925b670afdae5c9274b95b6c548c4c682b
tt <941375265@qq.com>
2019-02-23 11:37:29 +0800

    使用@Controller注解

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 * 搭建基本的mvc应用
 先下载了[Spring](https://repo.spring.io/libs-release-local/org/springframework/spring/5.0.1.RELEASE/ "5.0.1.RELEASE")的所有库到本地，搭了个最基本的web项目。
 在IDEA 上添加库的时候可以直接选择/WEB-INF/lib 目录，右击，选择**Add as Library..**
+
+* 使用@Controller注解,遇到错误 "通配符的匹配很全面, 但无法找到元素 'mvc:annotation-driven' 的声明"
+解决办法：
+spring-config.xml中，要添加 http://www.springframework.org/schema/mvc 和
+http://www.springframework.org/schema/mvc/spring-mvc.xsd
--- src/org/fkit/controller/HelloController.java
+++ src/org/fkit/controller/HelloController.java
@@ -1,6 +1,7 @@
 package org.fkit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,12 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class HelloController  {
 
-    @RequestMapping(value = "/hello")
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("handleRequest 被调用");
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("message","Hello nerd");
-        mv.setViewName("/WEB-INF/content/welcome.jsp");
-        return mv;
+
+    @RequestMapping("helloController")
+    public String helloController(Model model){
+        model.addAttribute("message","use @Controller");
+        return "welcome";
     }
 }
