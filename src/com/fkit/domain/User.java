78240e19be48b9fc8b95734f535d5fe048a79d51
tqw <941375265@qq.com>
2019-03-04 22:17:14 +0800

    文件的上传和下载，用了一个我不懂的ResponsedEntity对象

--- src/com/fkit/domain/User.java
+++ src/com/fkit/domain/User.java
@@ -1,11 +1,17 @@
 package com.fkit.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
     private String loginname;
     private String password;
     private String username;
+
+    // 对应上传的file，类型为MultipartFile，上传文件会自动绑定到image属性当中
+    private MultipartFile image;
+
     public User(){
 
     }
@@ -17,6 +23,14 @@ public class User implements Serializable {
                 ", password='" + password + '\'' +
                 ", username='" + username + '\'' +
                 '}';
+    }
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
     }
 
     public String getLoginname() {
